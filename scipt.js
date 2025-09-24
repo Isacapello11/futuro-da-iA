f3d6946a1db5ccbe26818f8e581cb0d1d5771bbe
Isacapello11 <141049333+Isacapello11@users.noreply.github.com>
2025-09-24 08:36:05 -0300

    Create scipt.js
    Signed-off-by: Isacapello11 <141049333+Isacapello11@users.noreply.github.com>

---
+++ scipt.js
@@ -0,0 +1,34 @@
+const caixaPrincipal = document.querySelector(caixa -
+    principal);
+
+const caixaPerguntas = document.querySelector(caixa -
+    perguntas);
+
+const caixaAlternativas = document.querySelector(caixa -
+    alternativas);
+
+const caixaResultado = document.querySelector(caixa -
+    resultado);
+
+const textoResultado = document.querySelector(texto -
+    resultado);
+
+const lista = [item1, item2]
+
+const perguntas = [
+    {
+        enunciado: "pergunta 1",
+        alternativas: ["Alternativa 1", "Alternativa 2"]
+    },
+    {
+        enunciado: "pergunta 2",
+        alternativas: ["alternativa 1", "alternativa 2"]
+    }
+]
+
+const lapis = {
+    tamanho: 20,
+    tipo: HB,
+    cor: Grafite,
+    temBorrachaAtras: false
+}
